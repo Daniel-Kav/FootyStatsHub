a6ff876658e66bcb53acc0b9174e13782f9195bd
daniel-kav <dankavatha01@gmail.com>
2024-03-08 20:43:32 +0300

    templates

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-url_patterns = [
-    path('',views.match_rosters_view, name='match_rosters'),
+urlpatterns = [
+    path('match-rosters/', views.match_rosters, name='match_rosters'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from understatapi import UnderstatClient
 
-# Create your views here.
+def match_rosters(request):
+    understat = UnderstatClient()
+    team_match_data = understat.team(team="Manchester_United").get_match_data(season="2019")
+    match_id = team_match_data[0]["id"]
+    roster_data = understat.match(match=match_id).get_roster_data()
+
+    context = {
+        'home_team': roster_data['home'],
+        'away_team': roster_data['away'],
+    }
+
+    return render(request, 'roster.html', context)
