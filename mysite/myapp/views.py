1ff4bf75352e29364f7494d8f69247edf0ab0b83
daniel-kav <dankavatha01@gmail.com>
2024-03-09 00:47:26 +0300

    setting limits

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,16 +5,16 @@ def match_rosters(request):
      # Use the UnderstatClient within a context manager to ensure proper resource management
     with UnderstatClient() as understat:
         # Fetch league player data for the English Premier League (EPL) season 2019
-        league_player_data = understat.league(league="EPL").get_player_data(season="2019")
+        league_player_data = understat.league(league="EPL").get_player_data(season="2019",limit= 2)
         
         # Fetch shot data for a specific player (player ID: 2371)
-        player_shot_data = understat.player(player="2371").get_shot_data()
+        player_shot_data = understat.player(player="2371").get_shot_data(limit= 2)
         
         # Fetch match data for the team Manchester United for the season 2019
-        team_match_data = understat.team(team="Manchester_United").get_match_data(season="2019")
+        team_match_data = understat.team(team="Manchester_United").get_match_data(season="2019",limit= 2)
         
         # Fetch roster data for a specific match (match ID: 14711)
-        roster_data = understat.match(match="14711").get_roster_data()
+        roster_data = understat.match(match="14711").get_roster_data(limit= 2)
 
     
     # Pass the fetched data to the template
